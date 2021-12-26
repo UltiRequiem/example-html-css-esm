287dc645ab280f011215c077508bb13a48bcf9ea
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-26 07:03:44 -0500

    :sparkles: Add Context box

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,6 @@ export function getAverage(...arr) {
   return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 
-
 export default function getCoolNumber() {
   return getAverage(...new Array({ length: 50 }).map(() => getRandomInt()));
 }
